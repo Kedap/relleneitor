faecb86020944a667dab6e12ce4b0b817bb17dda
Kedap <kedap.dev@protonmail.com>
2025-04-16 00:53:55 -0600

    format(examples): formato y quitando exportaciones sin usar

--- examples/proveedores_shema.py
+++ examples/proveedores_shema.py
@@ -1,16 +1,19 @@
 from src.schema import Table, Column, registry
 from src.generator import generate_insert_query
+
 # Esquema para la tabla 'proveedores'
 proveedores_table = Table(
     name="proveedores",
     columns=[
-        Column(name="rut", type="INTEGER", faker_provider="random_int", is_primary_key=True),
+        Column(
+            name="rut", type="INTEGER", faker_provider="random_int", is_primary_key=True
+        ),
         Column(name="nombre", type="TEXT", faker_provider="company"),
         Column(name="direccion", type="TEXT", faker_provider="address"),
         Column(name="telefono", type="INTEGER", faker_provider="phone_number"),
-        Column(name="pagina_web", type="TEXT", faker_provider="url")
+        Column(name="pagina_web", type="TEXT", faker_provider="url"),
     ],
-    primary_key="rut"
+    primary_key="rut",
 )
 
 num_rows = 10
--- examples/telefono_proveedor_schema.py
+++ examples/telefono_proveedor_schema.py
@@ -1,31 +1,34 @@
-from src.schema import Table, Column, ForeignKey, registry
+from src.schema import Table, Column, ForeignKey
 from src.generator import generate_insert_query
-from examples.proveedores_shema import proveedores_table
 
 telefono_proveedor_table = Table(
     name="telefono_proveedor",
     columns=[
-        Column(name="id", type="INTEGER", faker_provider="random_int", is_primary_key=True),
         Column(
-            name="proveedor", 
-            type="INTEGER", 
+            name="id", type="INTEGER", faker_provider="random_int", is_primary_key=True
+        ),
+        Column(
+            name="proveedor",
+            type="INTEGER",
             foreign_key=ForeignKey(
                 column="proveedor",
                 references_table="proveedores",
-                references_column="rut"
-            )
+                references_column="rut",
+            ),
         ),
         Column(name="casa", type="TEXT", faker_provider="phone_number"),
         Column(name="movil", type="TEXT", faker_provider="phone_number"),
         Column(name="oficina", type="TEXT", faker_provider="phone_number"),
-        Column(name="personal", type="TEXT", faker_provider="phone_number")
+        Column(name="personal", type="TEXT", faker_provider="phone_number"),
     ],
-    primary_key="id"
+    primary_key="id",
 )
 
 num_rows = 10
 
-sql_statements = generate_insert_query(table=telefono_proveedor_table, num_rows=num_rows)
+sql_statements = generate_insert_query(
+    table=telefono_proveedor_table, num_rows=num_rows
+)
 
 for stmt in sql_statements:
     print(stmt)
