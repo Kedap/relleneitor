d5e21ecefadc25965611d8242cc72a1118bfef3e
Kedap <kedap.dev@protonmail.com>
2025-04-18 09:32:53 -0600

    feat(examples/proveedores_shema_save_file): ejemplo para guardar archivo sql

---
+++ examples/proveedores_shema_save_file.py
@@ -0,0 +1,26 @@
+from src.schema import Table, Column, registry
+from src.generator import generate_insert_query
+from src.utils import export_sql_to_file
+
+# Esquema para la tabla 'proveedores'
+proveedores_table = Table(
+    name="proveedores",
+    columns=[
+        Column(
+            name="rut", type="INTEGER", faker_provider="random_int", is_primary_key=True
+        ),
+        Column(name="nombre", type="TEXT", faker_provider="company"),
+        Column(name="direccion", type="TEXT", faker_provider="address"),
+        Column(name="telefono", type="INTEGER", faker_provider="phone_number"),
+        Column(name="pagina_web", type="TEXT", faker_provider="url"),
+    ],
+    primary_key="rut",
+)
+
+num_rows = 10
+
+# Registrar la tabla en el registro global para uso con llaves foráneas
+registry.register(proveedores_table)
+
+sql_statements = generate_insert_query(table=proveedores_table, num_rows=num_rows)
+export_sql_to_file({"proveedores": sql_statements}, "proveedores.sql")
